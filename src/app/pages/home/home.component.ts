1029ef874ccc27c34bac483812912a8d95f020fa
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-19 19:17:06 +0200

    add food-class + update links

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/food.class';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { FoodService } from 'src/app/services/food.service';
 })
 export class HomeComponent implements OnInit  {
 
-  foods:String[] = [];
+  foods:Food[] = [];
 
   constructor(private foodService: FoodService){}
 
---
+++ src/app/shared/models/food.class.ts
@@ -0,0 +1,12 @@
+export class Food {
+  // ? = optional, ! = mandatory
+  id!: number;
+  name!: string;
+  price!: number;
+  tags?: string[];
+  favorite: boolean = false;
+  stars: number = 0;
+  imageUrl!: string;
+  origins!: string [];
+  cookTime!: string;
+}
